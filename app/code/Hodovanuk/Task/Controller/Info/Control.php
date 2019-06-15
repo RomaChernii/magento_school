6311b782565f2b0af6721585cfd2765bf436061a
mishagodovanuk <mishagodovanuk@gmail.com>
2019-06-15 19:24:07 +0300

    1.1_first_homework_hodovanuk

--- app/code/Hodovanuk/Task/Controller/Info/Control.php
+++ app/code/Hodovanuk/Task/Controller/Info/Control.php
@@ -1,9 +1,14 @@
 <?php
-
+/**
+ *
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 namespace Hodovanuk\Task\Controller\Info;
-use Magento\Framework\App\Action\Action as AbstractAccount;
 
-class Control extends AbstractAccount
+use Magento\Framework\App\Action\Action;
+
+class Control extends Action
 {
     public function execute()
     {
