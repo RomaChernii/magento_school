655c8ec7953fb1f7118176d6765372a4e0f6b7a0
k0mand <romadaaaa@gmail.com>
2019-06-17 16:12:07 +0300

    fidex -- was added a string which was missing in the end

--- app/code/Koshyk/HomeTaskModule/Controller/HomeTask/Info.php
+++ app/code/Koshyk/HomeTaskModule/Controller/HomeTask/Info.php
@@ -1,7 +1,13 @@
 <?php
-
+/**
+ *
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 namespace Koshyk\HomeTaskModule\Controller\HomeTask;
+
 use Magento\Framework\App\Action\Action as AbstractAction;
+
 class Info extends AbstractAction
 {
     public function execute()
