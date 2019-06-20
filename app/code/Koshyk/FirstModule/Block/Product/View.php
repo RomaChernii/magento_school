f7f4ea146572f53e19826ce1e2d451e9bc83a6b6
k0mand <romadaaaa@gmail.com>
2019-06-20 21:09:23 +0300

    Change block and template

---
+++ app/code/Koshyk/FirstModule/Block/Product/View.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Koshyk/FirstModule/Block/Product/View
+ *
+ * @category Koshyk
+ * @package Koshyk/FirstModule
+ * @author Roman Koshyk <romadaaaa@gmail.com>
+ */
+
+namespace Koshyk\FirstModule\Block\Product;
+
+use Magento\Catalog\Block\Product\View as MagentoView;
+
+/**
+ * Class Nordhing
+ *
+ * @package Koshyk\FirstModule\Block\Product
+ */
+class View extends MagentoView
+{
+    public function getProductDefaultQty($product = null)
+    {
+        return 15;
+    }
+}
