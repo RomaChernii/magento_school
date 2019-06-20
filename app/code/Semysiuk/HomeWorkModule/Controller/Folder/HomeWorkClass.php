dceedb8e74db281df07b38b2cf01056e544e8b19
DmitriySemysiuk <dmitriysemysiuk@gmail.com>
2019-06-20 19:58:44 +0300

    First home work

---
+++ app/code/Semysiuk/HomeWorkModule/Controller/Folder/HomeWorkClass.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Semysiuk\HomeWorkModule\Controller\Folder;
+
+use Magento\Framework\App\Action\Action;
+
+class HomeWorkClass extends Action
+{
+    public function execute()
+    {
+        echo "Hello World from Home";
+
+        exit();
+    }
+}
