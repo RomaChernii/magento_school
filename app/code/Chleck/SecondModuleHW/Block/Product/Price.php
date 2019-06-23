00dbcd03f9c57dec9eebf70086e83bbdd791ec08
YuriChleK <yurichlek@gmail.com>
2019-06-23 14:00:35 +0300

    New branch

---
+++ app/code/Chleck/SecondModuleHW/Block/Product/Price.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Chleck/SecondModuleHW/Block/Product/Product_view
+ *
+ * @category Chleck
+ * @package Chleck/SecondModuleHW/Product/Product_view
+ * @author Chleck Yuri
+ */
+
+namespace Chleck\SecondModuleHW\Block\Product;
+
+use Magento\Catalog\Block\Product\Price as MagentoPrise;
+
+class Price extends MagentoPrise
+{
+
+
+}
+
+
+
+
+
--- app/code/ivpel/homework/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by ivpel
- */
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'ivpel_homework',
-    __DIR__
-);
