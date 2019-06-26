f5ab2c39b85c4f4177961abcbe9b93c09f75c6d7
YuriChleK <yurichlek@gmail.com>
2019-06-26 20:33:23 +0300

    Home work

---
+++ app/code/Chleck/SecondModuleHW/Block/ChleckTest/Test.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Chleck\SecondModuleHW\Block\ChleckTest;
+use Magento\Catalog\Block\Product\View as MagentoView;
+
+
+/**
+ * Class Test
+ * @package Chleck\SecondModuleHW\Block\ChleckTest
+ */
+
+class Test extends MagentoView
+{
+    /**
+     * @return int|\Magento\Catalog\Model\Product
+     */
+
+   /* public function getProduct()
+    {
+        return "Hi";
+    }*/
+
+    public function hasOptions()
+    {
+
+        return "Hi";
+    }
+}
---
+++ app/code/Chleck/SecondModuleHW/Controller/ChleckIndexTest/IndexTest.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Chleck\SecondModuleHW\Controller\ChleckIndexTest;
+use Magento\Framework\App\Action\Action;
+
+/**
+ * Class Index
+ * @package Chleck\SecondModuleHW\Controller\Index
+ */
+
+class IndexTest extends Action
+{
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|void
+     */
+
+
+    public function execute(){
+
+        $this->_view->loadLayout();
+        $this->_view->renderLayout();
+        echo "Hi, it is my home work.)))";
+    }
+
+
+
+}
+
