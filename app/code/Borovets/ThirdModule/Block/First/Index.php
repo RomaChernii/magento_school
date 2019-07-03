d055613ba827397da7564b2a3ca85a65f403e9ee
sanbor96 <olexandr.borovets@gmail.com>
2019-07-04 01:17:04 +0300

    3.1 Home task with ViewModel by Borovets

---
+++ app/code/Borovets/ThirdModule/Block/First/Index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Borovets\ThirdModule\Block\First;
+
+use Magento\Framework\View\Element\Template;
+
+class Index extends Template
+{
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return __('Hello World!');
+    }
+    public function getMyUrl()
+    {
+        return $this->getUrl().'checkout/cart';
+    }
+}
