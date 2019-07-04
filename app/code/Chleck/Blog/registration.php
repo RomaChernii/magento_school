e92cb8a19e5903597ddd04ae4093ce6a9d5c7488
YuriChleK <yurichlek@gmail.com>
2019-07-04 20:13:03 +0300

    Class Work

--- app/code/Chleck/Blog/registration.php
+++ app/code/Chleck/Blog/registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Chleck_FifthModule',
+    'Chleck_Blog',
     __DIR__
 );
