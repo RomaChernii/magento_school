2ae2b46452704b81cceb6b2f714874d364eb11ce
mishagodovanuk <mishagodovanuk@gmail.com>
2019-07-04 21:01:55 +0300

    Get data helper

--- app/code/Hodovanuk/Config/Helper/Data.php
+++ app/code/Hodovanuk/Config/Helper/Data.php
@@ -3,17 +3,33 @@ namespace Hodovanuk\Config\Helper;
 
 use Magento\Framework\App\Helper\AbstractHelper;
 
+/**
+ * Class Data
+ * @package Hodovanuk\Config\Helper
+ */
 class Data extends AbstractHelper
 {
+    /**
+     *  Path to Yes/no value
+     */
     const IS_ENABLE_CONFIG_PATH = 'hodovanuk_section/hodovanuk_store_information/hodovanuk_field';
-    //Yes no
+
+    /**
+     *  Path to text field value
+     */
     const INPUT_VALUE_CONFIG_PATH = 'hodovanuk/second_field/value';
 
+    /**
+     * @return mixed
+     */
     public function getIsEnable()
     {
         return $this->scopeConfig->getValue(static::IS_ENABLE_CONFIG_PATH);
     }
 
+    /**
+     * @return bool|mixed
+     */
     public function getSecondValue()
     {
         $returnValue = false;
