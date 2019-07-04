8386448b8feaf13ce333cdc88e381c040c5d8c66
Victor Dehtiarov <4059858@gmail.com>
2019-07-04 20:12:08 +0300

    6.1_blog_dehtiarov

--- app/etc/config.php
+++ app/etc/config.php
@@ -8,6 +8,7 @@ return array (
     'Chleck_FirstModule' => 1,
     'Magento_Directory' => 1,
     'Magento_Eav' => 1,
+    'Dehtiarov_BlogVideh' => 1,
     'Magento_Config' => 1,
     'Dehtiarov_FirstModule' => 1,
     'Dehtiarov_HomeTaskVideh' => 1,
