4c978a5aaecdec0cde5872f0a14d87c1056cdcc9
WOL4ARA <andriy.skavronskiy@gmail.com>
2019-07-08 21:08:39 +0300

    Correct code style in Helper.

--- app/code/Skavronskiy/SamConfiguration/Helper/Data.php
+++ app/code/Skavronskiy/SamConfiguration/Helper/Data.php
@@ -8,9 +8,10 @@ class Data extends AbstractHelper
 {
     const IS_ENABLE_CONFIG_PATH ='sam_section/my_sam/sam_id';
     const INPUT_VALUE_CONFIG_PATH ='sam_section/my_sam/sam_input';
+
     public function getIsEnable()
     {
-     return $this->scopeConfig->getValue(static::IS_ENABLE_CONFIG_PATH);
+        return $this->scopeConfig->getValue(static::IS_ENABLE_CONFIG_PATH);
     }
 
     public function getValue()
@@ -18,6 +19,7 @@ class Data extends AbstractHelper
         $text = "";
         if($this->getIsEnable())
             $text = $this->scopeConfig->getValue(static::INPUT_VALUE_CONFIG_PATH);
+
         return $text;
     }
 }
