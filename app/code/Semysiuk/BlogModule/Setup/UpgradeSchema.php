86f4cab43e7b2b9cb488204f6a4b6468e26f27dc
DmitriySemysiuk <dmitriysemysiuk@gmail.com>
2019-07-08 16:09:46 +0300

    Update upgradeSchema

--- app/code/Semysiuk/BlogModule/Setup/UpgradeSchema.php
+++ app/code/Semysiuk/BlogModule/Setup/UpgradeSchema.php
@@ -5,6 +5,7 @@ namespace Semysiuk\BlogModule\Setup;
 use Magento\Framework\Setup\UpgradeSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Ddl\Table;
 
 class UpgradeSchema implements  UpgradeSchemaInterface
 {
@@ -14,7 +15,15 @@ class UpgradeSchema implements  UpgradeSchemaInterface
 
         if(version_compare($context->getVersion(), "0.0.2", "<"))
         {
-
+            $setup->getConnection()->addColumn(
+                $setup->getTable("semysiuk_blog_post"),
+                "description",
+                [
+                    'type' => Table::TYPE_TEXT,
+                    'length' => '64k',
+                    'comment' => 'Post Description'
+                ]
+            );
         }
 
         $setup->endSetup();
