4771d6bab399b1433cdc669ad4e60796b1140664
YuriChleK <yurichlek@gmail.com>
2019-07-10 20:43:06 +0300

    Create new table

--- app/code/Chleck/Blog/Setup/UpgradeSchema.php
+++ app/code/Chleck/Blog/Setup/UpgradeSchema.php
@@ -16,6 +16,7 @@ class UpgradeSchema implements UpgradeSchemaInterface
     /**
      * @param SchemaSetupInterface $setup
      * @param ModuleContextInterface $context
+     * @throws \Zend_Db_Exception
      */
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
@@ -31,6 +32,66 @@ class UpgradeSchema implements UpgradeSchemaInterface
                 ]
             );
         }
+       // $setup->endSetup();
+
+        if (version_compare($context->getVersion(), '1.0.2') < 0){
+
+        $table = $setup->getConnection()->newTable(
+            $setup->getTable('chleck_blog_post')
+        )->addColumn(
+            'Comment id',
+            Table::TYPE_INTEGER,
+            null,
+            [
+                'identity' => true,
+                'unsigned' => true,
+                'nullable' => false,
+                'primary' => true
+            ],
+            'Comment id'
+        )->addColumn(
+            'Post id',
+            Table::TYPE_TEXT,
+            255,
+            [],
+            'Post id'
+        )->addColumn(
+            'Name',
+            Table::TYPE_TEXT,
+            255,
+            [],
+            'User name'
+        )->addColumn(
+            'Surname',
+            Table::TYPE_TEXT,
+            255,
+            [],
+            'User surname'
+        )->addColumn(
+            'Email',
+            Table::TYPE_TEXT,
+            255,
+            [],
+            'User email'
+        )->addColumn(
+            'Comment',
+            Table::TYPE_TEXT,
+            255,
+            [],
+            'New comment'
+        )->addColumn(
+            'Answer',
+            Table::TYPE_TEXT,
+            255,
+            [],
+            'New answer'
+        )->setComment(
+            'Blog Post And Comment Table'
+        );
+
+        $setup->getConnection()->createTable($table);
+
+    }
         $setup->endSetup();
     }
 }
