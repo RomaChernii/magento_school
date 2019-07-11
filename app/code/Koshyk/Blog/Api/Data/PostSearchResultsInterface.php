9e7e452af685f05c78fd2356c4547bd2c3908278
k0mand <romadaaaa@gmail.com>
2019-07-11 22:20:48 +0300

    8.0 grid and repository Koshyk

---
+++ app/code/Koshyk/Blog/Api/Data/PostSearchResultsInterface.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Blog post search results interface
+ *
+ * @category  Koshyk
+ * @package   Koshyk\Blog
+ * @author    Roman Koshyk <romadaaaa@gmail.com>
+ */
+namespace Koshyk\Blog\Api\Data;
+
+use Magento\Framework\Api\SearchResultsInterface;
+
+/**
+ * Interface PostSearchResultsInterface
+ *
+ * @package Koshyk\Blog\Api\Data
+ */
+interface PostSearchResultsInterface extends SearchResultsInterface
+{
+    /**
+     * Get post list
+     *
+     * @return \Koshyk\Blog\Api\Data\PostInterface[]
+     */
+    public function getItems();
+
+    /**
+     * Set post list
+     *
+     * @param \Koshyk\Blog\Api\Data\PostInterface[] $items
+     *
+     * @return PostSearchResultsInterface
+     */
+    public function setItems(array $items);
+}
