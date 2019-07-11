7d932a79165775e620dfb83c32dbc24f5a5b53b1
k0mand <romadaaaa@gmail.com>
2019-07-11 14:04:58 +0300

    7.0 menu and hometask Koshyk

--- app/code/Koshyk/Blog/Setup/UpgradeSchema.php
+++ app/code/Koshyk/Blog/Setup/UpgradeSchema.php
@@ -10,12 +10,14 @@ class UpgradeSchema implements UpgradeSchemaInterface
 {
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        $setup->startSetup();
+        $installer = $setup;
+        $connection = $setup->getConnection();
+
+        $installer->startSetup();
 
         if (version_compare($context->getVersion(), '0.0.3', '<')) {
-            $connection = $setup->getConnection();
             $connection->addColumn(
-                $setup->getTable('koshyk_blog_post'),
+                $installer->getTable('koshyk_blog_post'),
                 'description',
                [
                     'type' => Table::TYPE_TEXT,
@@ -24,6 +26,88 @@ class UpgradeSchema implements UpgradeSchemaInterface
                ]
             );
         }
-        $setup->endSetup();
+
+        if((version_compare($context->getVersion(), '0.0.4') < 0) ){
+            $table = $connection->newTable(
+                $installer->getTable('koshyk_blog_comment')
+            )->addColumn(
+                'id',
+                Table::TYPE_INTEGER,
+                null,
+                [
+                    'identity' => true,
+                    'unsigned' => true,
+                    'nullable' => false,
+                    'primary' => true
+                ],
+                'Comment id'
+            )->addColumn(
+                'first_name',
+                Table::TYPE_TEXT,
+                255,
+                [],
+                'Creator first name'
+            )->addColumn(
+                'last_name',
+                Table::TYPE_TEXT,
+                255,
+                [],
+                'Creator last name'
+            )->addColumn(
+                'date',
+                Table::TYPE_TIMESTAMP,
+                null,
+                [
+                    'nullable' => false,
+                    'default' => Table::TIMESTAMP_INIT
+                ],
+                'Comment created at'
+            )->addColumn(
+                'email',
+                Table::TYPE_TEXT,
+                255,
+                [],
+                'Creator email'
+            )->addColumn(
+                'comment',
+                Table::TYPE_TEXT,
+                '2M',
+                [],
+                'Creator comment'
+            )->addColumn(
+                'answer',
+                Table::TYPE_SMALLINT,
+                null,
+                [
+                    'nullable' => false,
+                    'default' => '1'
+                ],
+                'Comment status'
+            )->addColumn(
+                'post_id',
+                Table::TYPE_INTEGER,
+                null,
+                [
+                    'unsigned' => true,
+                    'nullable' => false
+                ],
+                'post foreignkey'
+            )->addForeignKey(
+                $installer->getFkName(
+                    'koshyk_blog_comment',
+                    'post_id',
+                    'koshyk_blog_post',
+                    'id'),
+                'post_id',
+                $installer->getTable('koshyk_blog_post'),
+                'id',
+                Table::ACTION_CASCADE
+            )->setComment(
+                'Blog Comment Table'
+            );
+            $installer->getConnection()
+                ->createTable($table);
+        }
+        $installer->endSetup();
     }
 }
