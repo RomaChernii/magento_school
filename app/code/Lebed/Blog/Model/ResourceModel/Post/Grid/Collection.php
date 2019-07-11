07cd02844cf54ee6f15cdc1544689a9281a847e8
Tetiana Lebed <teleb@smile.fr>
2019-07-11 20:10:36 +0300

    8.1 Createt grid and repository Lebed

---
+++ app/code/Lebed/Blog/Model/ResourceModel/Post/Grid/Collection.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Lebed Blog grid post collection
+ *
+ * @category  Lebed
+ * @package   Lebed\Blog
+ * @author    Tetiana Lebed <teleb@smile.fr>
+ * @copyright 2019 Smile
+ */
+namespace Lebed\Blog\Model\ResourceModel\Post\Grid;
+
+use Magento\Framework\Api\Search\AggregationInterface;
+use Magento\Framework\Api\Search\SearchResultInterface;
+use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Framework\View\Element\UiComponent\DataProvider\Document;
+use Lebed\Blog\Model\ResourceModel\Post;
+use Lebed\Blog\Model\ResourceModel\Post\Collection as PostCollection;
+
+/**
+ * Class Collection
+ *
+ * @package Lebed\Blog\Model\ResourceModel\Post\Grid\Collection
+ *
+ */
+class Collection extends PostCollection implements SearchResultInterface
+{
+    /**
+     * Aggregations
+     *
+     * @var AggregationInterface
+     */
+    private $aggregations;
+
+    /**
+     * Collection constructor
+     *
+     * @return void
+     */
+    public function _construct()
+    {
+        $this->_init(Document::class, Post::class);
+    }
+
+    /**
+     * Get aggregations
+     *
+     * @return AggregationInterface
+     */
+    public function getAggregations()
+    {
+        return $this->aggregations;
+    }
+
+    /**
+     * Set aggregations
+     *
+     * @param AggregationInterface $aggregations
+     *
+     * @return void
+     */
+    public function setAggregations($aggregations)
+    {
+        $this->aggregations = $aggregations;
+    }
+
+    /**
+     * Get search criteria
+     *
+     * @return \Magento\Framework\Api\SearchCriteriaInterface|null
+     */
+    public function getSearchCriteria()
+    {
+        return null;
+    }
+
+    /**
+     * Set search criteria
+     *
+     * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
+     *
+     * @return $this
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setSearchCriteria(SearchCriteriaInterface $searchCriteria = null)
+    {
+        return $this;
+    }
+
+    /**
+     * Get total count
+     *
+     * @return int
+     */
+    public function getTotalCount()
+    {
+        return $this->getSize();
+    }
+
+    /**
+     * Set total count
+     *
+     * @param int $totalCount
+     *
+     * @return $this
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setTotalCount($totalCount)
+    {
+        return $this;
+    }
+
+    /**
+     * Set items list
+     *
+     * @param \Magento\Framework\Api\ExtensibleDataInterface[] $items
+     *
+     * @return $this
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setItems(array $items = null)
+    {
+        return $this;
+    }
+}
