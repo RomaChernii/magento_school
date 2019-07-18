578410f3e496ee5bc0ac7566044276126a998b38
Victor Dehtiarov <4059858@gmail.com>
2019-07-18 20:44:07 +0300

    frontent on blog was created

---
+++ app/code/Dehtiarov/BlogVideh/Controller/Blog/Post/View.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Dehtiarov Blog controller post index
+ *
+ * @category  Dehtiarov
+ * @package   Dehtiarov\BlogVideh
+ * @author    Dehtiarov Victor <videh@smile.fr>
+ * @copyright 2019 Smile
+ */
+namespace Dehtiarov\BlogVideh\Controller\Blog\Post;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Context;
+
+/**
+ * Class View
+ *
+ * @package Dehtiarov\BlogVideh\Controller\Blog\Post
+ */
+class View extends Action
+{
+    /**
+     * Page factory
+     *
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * View constructor
+     *
+     * @param Context     $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+
+        return $resultPage;
+    }
+}
