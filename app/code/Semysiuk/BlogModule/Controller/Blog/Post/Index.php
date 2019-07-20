31c8be4f8c468269e6a6b0961d723184d50d3d93
DmitriySemysiuk <dmitriysemysiuk@gmail.com>
2019-07-20 12:18:49 +0300

    Add frontend - listing and view of posts

---
+++ app/code/Semysiuk/BlogModule/Controller/Blog/Post/Index.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Semysiuk\BlogModule\Controller\Blog\Post;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Context;
+
+/**
+ * Class Index
+ *
+ * @package Semysiuk\BlogModule\Controller\Blog\Post
+ */
+class Index extends Action
+{
+    /**
+     * Page factory
+     *
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor
+     *
+     * @param Context     $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+
+        return $resultPage;
+    }
+}
+
