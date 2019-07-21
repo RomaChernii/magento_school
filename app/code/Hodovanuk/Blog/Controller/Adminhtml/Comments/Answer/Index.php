a70e8986bff52cc547173b1642118ea711e0ac42
mishagodovanuk <mishagodovanuk@gmail.com>
2019-07-21 20:31:54 +0300

    add action answer

---
+++ app/code/Hodovanuk/Blog/Controller/Adminhtml/Comments/Answer/Index.php
@@ -0,0 +1,55 @@
+<?php
+namespace Hodovanuk\Blog\Controller\Adminhtml\Comments\Answer;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Index
+ *
+ * @package Hodovanuk\Blog\Controller\Adminhtml\Comments
+ */
+class Index extends Action
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Hodovanuk_Blog::comments';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Hodovanuk_Blog::comments');
+        $resultPage->addBreadcrumb(__('Blog Comments'), __('Blog Comments'));
+        $resultPage->addBreadcrumb(__('Blog Comments'), __('Blog Comments'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Blog Comments'));
+
+        return $resultPage;
+    }
+}
