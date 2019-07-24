b3822a44ed797678e32d2b18a394dadd2cc8e7a0
DmitriySemysiuk <dmitriysemysiuk@gmail.com>
2019-07-24 13:54:24 +0300

    Add form and frontend posts/comments

--- app/code/Semysiuk/BlogModule/Block/Post/Form.php
+++ app/code/Semysiuk/BlogModule/Block/Post/Form.php
@@ -6,19 +6,17 @@ use Magento\Framework\View\Element\Template;
 
 class Form extends Template
 {
-    protected $postId;
 
-    public function __construct(Template\Context $context, array $data = [])
-    {
-        parent::__construct($context, $data);
-
-        $this->postId = $this->getRequest()->getParam("id");
-
-    }
+//    public function __construct(Template\Context $context, array $data = [])
+//    {
+//        parent::__construct($context, $data);
+//
+//        $this->postId = $this->getRequest()->getParam("id");
+//
+//    }
 
     public function getPostId()
     {
-        //var_dump($this->postId);
-        return $this->postId[0];
+        return $this->getRequest()->getParam("id");
     }
 }
---
+++ app/code/Semysiuk/BlogModule/Controller/Blog/Comment/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Semysiuk\BlogModule\Controller\Blog\Comment;
+
+use Magento\Framework\App\Action\Action;
+
+
+class Index extends Action
+{
+
+}
