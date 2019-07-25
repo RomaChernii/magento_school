04f6f2d6dddfe00d3c06193a5a163eec9c4a2602
Roman CHERNII <roche@smile.fr>
2019-07-25 18:11:32 +0300

    add customer attribute

---
+++ app/code/Roche/Customer/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Roche Customer registration
+ *
+ * @category  Roche
+ * @package   Roche\Customer
+ * @author    Roman Chernii <roche@smile.fr>
+ * @copyright 2019 Smile
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Roche_Customer',
+    __DIR__
+);
