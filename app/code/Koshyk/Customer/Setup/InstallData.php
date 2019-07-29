4e5ee4ee3e80b675a6ee6705a3791df05211a0fe
k0mand <romadaaaa@gmail.com>
2019-07-29 11:23:06 +0300

    12.0_add_customer_attribute_Koshyk

---
+++ app/code/Koshyk/Customer/Setup/InstallData.php
@@ -0,0 +1,97 @@
+<?php
+namespace Koshyk\Customer\Setup;
+
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Api\AttributeRepositoryInterface;
+use Magento\Customer\Model\Customer;
+
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * EAV setup factory
+     *
+     * @var \Magento\Eav\Setup\EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Customer setup factory
+     *
+     * @var CustomerSetupFactory
+     */
+    private $customerSetupFactory;
+
+    /**
+     * Attribute repository interface
+     *
+     * @var AttributeRepositoryInterface
+     */
+    private $attributeRepository;
+
+    /**
+     * Constructor InstallData
+     *
+     * @param EavSetupFactory              $eavSetupFactory
+     * @param CustomerSetupFactory         $customerSetupFactory
+     * @param AttributeRepositoryInterface $attributeRepository
+     */
+    public function __construct(
+        EavSetupFactory $eavSetupFactory,
+        CustomerSetupFactory $customerSetupFactory,
+        AttributeRepositoryInterface $attributeRepository
+    ) {
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeRepository = $attributeRepository;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+
+        $customerSetup->addAttribute(
+            Customer::ENTITY,
+            'people_check',
+            [
+                'type'           => 'text',
+                'label'          => 'People check',
+                'input'          => 'text',
+                'required'       => 0,
+                'visible'        => 1,
+                'sort_order'     => 50,
+                'position'       => 50,
+                'source'         => '',
+                'validate_rules' => '',
+                'system'         => 0,
+                'frontend_label' => 'Who are you?',
+                'backend_model'  => ''
+            ]
+        );
+
+        // show the attribute in the following forms
+        $attribute = $customerSetup
+            ->getEavConfig()
+            ->getAttribute(
+                Customer::ENTITY,
+                'people_check'
+            )->addData(
+                [
+                    'used_in_forms' => [
+                        'adminhtml_customer',
+                        'adminhtml_checkout',
+                        'customer_account_create',
+                        'customer_account_edit'
+                    ]
+                ]
+            );
+
+        $this->attributeRepository->save($attribute);
+
+        $setup->endSetup();
+    }
+}
---
+++ app/code/Koshyk/Customer/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Koshyk Customer registration
+ *
+ * @category  Koshyk
+ * @package   Koshyk\Blog
+ * @author    Roman Koshyk <romadaaaa@gmail.com>
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Koshyk_Customer',
+    __DIR__
+);
