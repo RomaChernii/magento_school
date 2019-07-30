d7885d3b9763c3bb29627f51cf684485d3b4c93e
Roman CHERNII <roche@smile.fr>
2019-07-30 18:22:38 +0300

    create own theme and requireJs

---
+++ app/design/frontend/Roche/default/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Roche registration file theme
+ *
+ * @category  Roche
+ * @package   Roche\Default
+ * @author    Roman Chernii <roche@smile.fr>
+ * @copyright 2019 Smile
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Roche/default',
+    __DIR__
+);
