5dc2bab372aeb3e96dfc6e8fbfbe4ba5fa0adbb5
Tetiana Lebed <teleb@smile.fr>
2019-07-30 20:31:54 +0300

    Created theme

---
+++ app/design/frontend/Lebed/default/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Lebed registration file theme
+ *
+ * @category  Lebed
+ * @package   Lebed\Default
+ * @author    Tetiana Lebed<teleb@smile.fr>
+ * @copyright 2019 Smile
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Lebeds/default',
+    __DIR__
+);
