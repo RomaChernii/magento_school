02f61f2ac1750fbf08374eb1c8f70b1902daac74
Tetiana Lebed <teleb@smile.fr>
2019-08-04 13:46:07 +0300

    13.0 requirejs Lebed

---
+++ app/code/Lebed/SecondHomeModule/view/frontend/requirejs-config.js
@@ -0,0 +1,15 @@
+/**
+ * Lebed requirejs-config.js
+ *
+ * @category  Lebed
+ * @package   Lebed\SecondHomeModule
+ * @author    Tetiana Lebed <teleb@smile.fr>
+ * @copyright 2019 Smile
+ */
+var config = {
+    map: {
+        '*': {
+            buttonAlertLebed: 'Lebed_SecondHomeModule/js/button-alert'
+        }
+    }
+};
